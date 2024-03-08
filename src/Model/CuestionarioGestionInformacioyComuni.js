abc1fbb4581aa00053e31e45a448b6dcbb8f1ac4
Alex-Pablo <alexpabloanibal@gmail.com>
2024-03-08 17:18:01 -0600

    cuestionario de gestion de informacion y comunicacon

---
+++ src/Model/CuestionarioGestionInformacioyComuni.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const CuestionarioGestionInformacioyComuni = mongoose.Schema(
+  {
+    pregunta1: String,
+    pregunta2: String,
+    pregunta3: String,
+    pregunta4: String,
+    pregunta5: String,
+    pregunta6: String,
+    pregunta7: String,
+    pregunta8: String,
+    pregunta9: String,
+    idUser: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.CuestionarioGestionInformacioyComuni || mongoose.model("CuestionarioGestionInformacioyComuni", CuestionarioGestionInformacioyComuni);
---
+++ src/app/api/cuestionarioGestionInformacioyComuni/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server"
+import cuestionarioGestionInformacioyComuni from "../../../Model/CuestionarioGestionInformacioyComuni"
+import connectDB from "../../lib/connectDB"
+
+
+export async function POST(req){
+    
+    await connectDB()
+    const data = await req.json()
+    const {pregunta1,pregunta2,pregunta3,pregunta4,pregunta5,pregunta6,pregunta7,pregunta8, pregunta9, idUser} = data
+
+    console.log(data)
+    const person = new cuestionarioGestionInformacioyComuni({
+        pregunta1: pregunta1,
+        pregunta2: pregunta2,
+        pregunta3: pregunta3,
+        pregunta4:pregunta4,
+        pregunta5 :pregunta5,
+        pregunta6: pregunta6,
+        pregunta7 :pregunta7,
+        pregunta8 :pregunta8,
+        pregunta9:pregunta9,
+        idUser:idUser
+    })
+    await person.save()
+
+    return NextResponse.json({
+        message:"ok"
+    })
+}
